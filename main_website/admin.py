90c77f8dbd98ea5376832d4e7cbf6c1e46eea641
Ahmed Zaher <ahmedzaher5788@gmail.com>
2023-05-14 21:27:16 +0300

    Add Grades model, some dummy data

--- main_website/admin.py
+++ main_website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Department, Course, Student
+from .models import Department, Course, Student, Grades
 
 admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Student)
+admin.site.register(Grades)
