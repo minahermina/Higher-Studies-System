e807df2134c255fdb305b052ad7921505055d8cc
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2023-05-18 21:48:38 +0200

    edit in menu in header & add getting parameter while redirecting to edit student page

--- main_website/views.py
+++ main_website/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Student, Grades
 
-def home(request):
 
+def home(request):
     return render(request, 'main_website/home.html', {})
 
-def loginStudent(request):
 
+def loginStudent(request):
     return render(request, 'main_website/login_student.html', {})
 
-def loginAdmin(request):
 
+def loginAdmin(request):
     return render(request, 'main_website/login_admin.html', {})
+
+
 def registered_courses(request):
     grades = Grades.objects.filter(student_id='20210031')
 
     context = {
-        'grades' : Grades.objects.filter
+        'grades': Grades.objects.filter
     }
     return render(request, 'main_website/registered_courses.html', context)
 
@@ -24,19 +26,27 @@ def registered_courses(request):
 def search_students(request):
     students = Student.objects.all()
     context = {
-        'students' : students
+        'students': students
     }
-    return render(request , 'main_website/search.html' , context)
+    return render(request, 'main_website/search.html', context)
+
 
 def add_course(request):
-    return render(request , 'main_website/add_course.html',{})
+    return render(request, 'main_website/add_course.html', {})
+
 
 def edit_student(request):
-    student = Student.objects.filter(stud_id='20210031')
+    id = request.GET.get('student_id')
+
+    student = Student.objects.filter(stud_id=id)
+
+    if request.method == 'POST':
+        print(request.POST)
     context = {
         'student': student
     }
     return render(request, 'main_website/edit_student.html', context)
+
 
 def error_404(request, exception):
     return render(request, 'main_website/404.html', status=404)
