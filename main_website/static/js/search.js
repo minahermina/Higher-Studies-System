c692fbbb8a2d2d768e66438123c1d7b4bda2a8c3
AbdoAlshoki2 <abdelrahmanalshoki@gmail.com>
2023-05-24 16:58:41 +0300

    added delete events

--- main_website/static/js/search.js
+++ main_website/static/js/search.js
@@ -1,12 +1,7 @@
-    var deleteButtons = document.querySelectorAll(".delete");
 
-    // Attach click event handler to each delete button
-    deleteButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            if (confirm("Are you sure you want to delete this student?")) {
-                // Find the closest delete-form ancestor of the clicked button
-                var deleteForm = button.closest(".delete-form");
-                deleteForm.submit();
-            }
-        });
-    });
+
+document.getElementById("deleteButton").addEventListener("click", function() {
+    if (confirm("Are you sure you want to delete this student?")) {
+        document.getElementById("deleteForm").submit();
+    }
+});
