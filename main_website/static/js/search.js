eb34ce458099dcb7c1231e25b674debb0f748348
AbdoAlshoki2 <abdelrahmanalshoki@gmail.com>
2023-05-25 17:57:46 +0300

    edit the search.js and put a function confirmation & edit in search.html file

--- main_website/static/js/search.js
+++ main_website/static/js/search.js
@@ -1,14 +1,4 @@
 
-
-var deleteButtons = document.querySelectorAll(".delete");
-
-    // Attach click event handler to each delete button
-    deleteButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            if (confirm("Are you sure you want to delete this student?")) {
-                // Find the closest delete-form ancestor of the clicked button
-                var deleteForm = button.closest(".delete-form");
-                deleteForm.submit();
-            }
-        });
-    });
+function confirmation() {
+    return confirm("Are you sure you want to delete this student?");
+}
