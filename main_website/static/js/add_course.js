56c0dc137879760e4981ac100796db19083a3a17
Sama Ahmed <102187124+SamaAhmedS@users.noreply.github.com>
2023-05-26 05:03:45 +0300

    Update add_course.js

--- main_website/static/js/add_course.js
+++ main_website/static/js/add_course.js
@@ -1,11 +1,4 @@
 
-let storedCourses = JSON.parse(localStorage.getItem("courses")) || [];
-
-let coursesID=[];
-for(let i=0;i<storedCourses.length;i++){
-    coursesID.push(storedCourses[i].id);
-}
-
 function validInputs(){
     const cName = document.getElementById("courseName").value;
     var letters = /^[A-Za-z\s]+[0-9]?$/; // added space to regex
@@ -27,17 +20,8 @@ function validInputs(){
     else{
         document.getElementById('idError').style.display = "none";
     }
-    if(coursesID.includes(cID)){
-        document.getElementById("idError").innerText="There is a course with this ID"; 
-        document.getElementById('idError').style.display = "block";
-        document.getElementById("courseID").focus();
-        return false;
-    }
-    else{
-        document.getElementById('idError').style.display = "none";
-    }
     var department = document.getElementById("department").value;
-    if (department == "") {
+    if (department === "") {
         //document.getElementById('departmentError').innerHTML = "Please select a department.";
         document.getElementById('departmentError').style.display = "block";
         document.getElementById("department").focus();
@@ -52,49 +36,13 @@ function validInputs(){
  const form = document.getElementById('form');
 
   form.addEventListener('submit', function(event) {
-      event.preventDefault(); // prevent default form submission behavior
+
     if(validInputs()){
         alert("Course added successfully!");
-    // Get form values
-    const courseName = document.getElementById("courseName").value;
-    const courseID = document.getElementById("courseID").value;
-    const courseHours = document.getElementById("courseHours").value;
-    const department = document.getElementById("department").value;
-    const lectureDay = document.getElementById("lecDay").value;
-    const hallNumber = document.getElementById("hNumber").value;
-    // Create an object to store the form data
-    let course = {
-        name: courseName,
-        id: courseID,
-        hours: courseHours,
-        department: department,
-        day: lectureDay,
-        hall: hallNumber
-    };
-    storedCourses.push(course);
-    // Save the form data to local storage
-    localStorage.setItem("courses", JSON.stringify(storedCourses));
 
-    var departments =JSON.parse(localStorage.getItem('departments'));
-        
-    if(department=="CS"){
-        departments[0].push(courseID);
     }
-    if(department=="IS"){
-        departments[1].push(courseID);
-    }
-    if(department=="OR"){
-        departments[2].push(courseID);
-    }
-    if(department=="IT"){
-        departments[3].push(courseID);
-    }
-    if(department=="AI"){
-        departments[4].push(courseID);
-    }
-    localStorage.setItem('departments', JSON.stringify(departments));
-    location.reload();
-    
+    else{// prevent default form submission behavior
+        event.preventDefault();
     }
 });
 
