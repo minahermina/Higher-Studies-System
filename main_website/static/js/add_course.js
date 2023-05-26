e14925ea77d874f267ecd2a10bff3f6ff55729ee
Sama Ahmed <102187124+SamaAhmedS@users.noreply.github.com>
2023-05-26 17:49:15 +0300

    Update add_course.js

--- main_website/static/js/add_course.js
+++ main_website/static/js/add_course.js
@@ -1,7 +1,7 @@
 
 function validInputs(){
     const cName = document.getElementById("courseName").value;
-    var letters = /^[A-Za-z\s]+[0-9]?$/; // added space to regex
+    var letters = /^[A-Za-z\s]+[0-9]*$/;// added space to regex
     if(!cName.match(letters) ){
         document.getElementById('cNameError').style.display = "block";
         document.getElementById("courseName").focus();
@@ -11,7 +11,7 @@ function validInputs(){
         document.getElementById('cNameError').style.display = "none";
     }
     var cID=document.getElementById("courseID").value; 
-    var m =/^([a-zA-Z])+([0-9])+$/
+    var m =/^[A-Za-z\s]+[0-9]+$/
     if (!cID.match(m)){  
         document.getElementById('idError').style.display = "block";
         document.getElementById("courseID").focus();
@@ -22,13 +22,22 @@ function validInputs(){
     }
     var department = document.getElementById("department").value;
     if (department === "") {
-        //document.getElementById('departmentError').innerHTML = "Please select a department.";
-        document.getElementById('departmentError').style.display = "block";
+        document.getElementById('depError').style.display = "block";
         document.getElementById("department").focus();
         window.scroll(10,10);
         return false;
     }else{
         document.getElementById('departmentError').style.display = "none";
+    }
+
+    var lDay = document.getElementById("lecDay").value;
+    if (lDay === "") {
+        document.getElementById('LError').style.display = "block";
+        document.getElementById("lecDay").focus();
+        window.scroll(10,10);
+        return false;
+    }else{
+        document.getElementById('LError').style.display = "none";
     }
     return true;
 }
@@ -37,12 +46,12 @@ function validInputs(){
 
   form.addEventListener('submit', function(event) {
 
-    if(validInputs()){
-        alert("Course added successfully!");
-
-    }
-    else{// prevent default form submission behavior
+    if(!validInputs()){
+        // prevent default form submission behavior
         event.preventDefault();
+    }
+    else{
+        alert("Course added successfully!");
     }
 });
 
