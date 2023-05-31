c631df0f83a929201dc306dfa1d8e585f1af1a26
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2023-05-31 07:08:29 +0200

    test

--- Higher_studies/settings.py
+++ Higher_studies/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = 'static/'
-STATIC_URL = STATIC_ROOT
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 
